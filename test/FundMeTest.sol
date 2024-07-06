7b18ee2f653d93a8b7568b0fe9cfa8d987c85a34
xletme <2485592098@qq.com>
2024-07-06 16:29:15 +0800

    our first commmit

---
+++ test/FundMeTest.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testDemo() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+
+}
